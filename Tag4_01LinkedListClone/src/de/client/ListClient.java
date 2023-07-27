ef7d4fa96ac5992e577389e0e44daca9ff8b9067
Jo Wagner <jowagner@limago.de>
2023-07-27 11:03:32 +0200

    initial commit

--- Tag4_01LinkedListClone/src/de/client/ListClient.java
+++ Tag4_01LinkedListClone/src/de/client/ListClient.java
@@ -3,7 +3,14 @@ package de.client;
 import de.goodyear.collections.List;
 import de.tiere.Schwein;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class ListClient {
+    private enum Command {Ende, Append,Get,Update,Remove,MoveFirst,MoveLast,MovePrevious,MoveNext, Status};
+    private final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
     private final List<Schwein> liste;
 
@@ -11,7 +18,53 @@ public class ListClient {
         this.liste = liste;
     }
 
+
+
+    private Schwein createSchwein() {
+        try {
+            System.out.print("Bitte geben Sie dem Schwein einen Namen: ");
+            String name = in.readLine();
+            return new Schwein(name);
+        } catch (IOException e) {
+            return new Schwein("Zombieschwein");
+        }
+    }
+
     public void start() {
 
+
+        do {
+            switch (showMenu()) {
+                case Append -> liste.append(createSchwein());
+                case Get -> System.out.println(liste.get());
+                case Update-> liste.update(createSchwein());
+                case Remove-> liste.remove();
+                case MoveFirst->liste.moveToTail();
+                case MoveLast->liste.moveToTail();
+                case MovePrevious->liste.movePrevious();
+                case MoveNext->liste.moveNext();
+                case Status->System.out.println("Empty=" + liste.isEmpty() + ", Bof=" + liste.isBeginOfList() + ", Eof=" + liste.isEndOfList());
+                case Ende -> {return;}
+
+            }
+
+        } while (true);
+    }
+
+    private Command showMenu() {
+        do {
+            try {
+
+
+                for (int i = 0 ; i < Command.values().length; i ++ ) {
+                    System.out.println(i + "................" + Command.values()[i]);
+                }
+
+                return Command.values()[Integer.valueOf(in.readLine())];
+            } catch (Throwable e) {
+                System.err.println("Unbekannter Befehl");
+
+            }
+        } while (true);
     }
 }
