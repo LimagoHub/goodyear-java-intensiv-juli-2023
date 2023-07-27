24ad2254d54d7a99db949ef7f5a1187d88f6e420
Jo Wagner <jowagner@limago.de>
2023-07-27 09:01:49 +0200

    initial commit

--- Tag3_02PaintClone/src/de/gui/MyMenuBar.java
+++ Tag3_02PaintClone/src/de/gui/MyMenuBar.java
@@ -1,26 +1,113 @@
 package de.gui;
 
-import java.awt.*;
 
+import java.util.LinkedHashMap;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+import static de.gui.PaintClone.Form.*;
 public class MyMenuBar extends MenuBar {
 
-    public MyMenuBar()  {
+    private final PaintClone paintClone;
+    private Menu currentMenu;
 
-        Menu menu;
-        MenuItem menuItem;
+    public MyMenuBar(final PaintClone paintClone)  {
 
-        menu = new Menu("Datei");
-        menuItem = new MenuItem("Speichern");
-        menu.add(menuItem);
-        menuItem = new MenuItem("Laden");
-        menu.add(menuItem);
-        menu.addSeparator();
-        menuItem = new MenuItem("Beenden");
-        menu.add(menuItem);
-        add(menu);
-
-        menu = new Menu("Farben");
-        add(menu);
+        this.paintClone = paintClone;
+        createMenuBar();
 
     }
+
+    private void createMenuBar() {
+        createMenuStructureMap().forEach(this::createMenu);
+    }
+
+    private void createMenu(final String label, final Map<String, ActionListener> itemMap) {
+        currentMenu = new Menu(label);
+        itemMap.forEach(this::createMenuItem);
+        add(currentMenu);
+    }
+
+    private void createMenuItem(final String label, final ActionListener actionListener){
+        var menuItem = new MenuItem(label);
+        menuItem.addActionListener(actionListener);
+        currentMenu.add(menuItem);
+    }
+
+    private Map<String, Map<String, ActionListener>> createMenuStructureMap() {
+//        return Map.ofEntries(
+//                entry("Datei", Map.ofEntries(
+//                        entry("Speichern", e->paintClone.save()),
+//                        entry("Laden", e->paintClone.load()),
+//                        entry("Beenden", e->paintClone.close())
+//                ))
+//                ,
+//                entry("Formen", Map.ofEntries(
+//                        entry("Linie", e->paintClone.setForm(LINE)),
+//                        entry("Oval", e->paintClone.setForm(OVAL)),
+//                        entry("Rechteck", e->paintClone.setForm(RECTANGLE)),
+//                        entry("Loeschen", e->paintClone.setForm(CLEAR))
+//
+//                ))
+//                ,
+//                entry("Farben", Map.ofEntries(
+//                        entry("Weiss", e->paintClone.setColor(Color.WHITE)),
+//                        entry("Rot", e->paintClone.setColor(Color.RED)),
+//                        entry("Gruen", e->paintClone.setColor(Color.GREEN)),
+//                        entry("Blau", e->paintClone.setColor(Color.BLUE)),
+//                        entry("Gelb", e->paintClone.setColor(Color.YELLOW)),
+//                        entry("Schwarz", e->paintClone.setColor(Color.BLACK)),
+//                        entry("Pink", e->paintClone.setColor(Color.PINK))
+//
+//                ))
+//        );
+
+
+
+
+        final var result = new LinkedHashMap<String, Map<String, ActionListener>>();
+        result.put("Datei", createDateiItems());
+        result.put("Formen", createFormenItems());
+        result.put("Farben", createFarbenItems());
+        return result;
+    }
+
+
+    private Map<String, ActionListener> createDateiItems() {
+        final var dateiItems = new LinkedHashMap<String, ActionListener>();
+        dateiItems.put("Speichern", e->paintClone.save());
+        dateiItems.put("Laden", e->paintClone.load());
+        dateiItems.put("Beenden", e->paintClone.close());
+        return dateiItems;
+    }
+    private Map<String, ActionListener> createFormenItems() {
+        final var formenItems = new LinkedHashMap<String, ActionListener>();
+        formenItems.put("Linie",    e->setForm(LINE));
+        formenItems.put("Oval",     e->setForm(OVAL));
+        formenItems.put("Rechteck", e->setForm(RECTANGLE));
+        formenItems.put("Loeschen", e->setForm(CLEAR));
+        return formenItems;
+    }
+
+    private Map<String, ActionListener> createFarbenItems() {
+        final var farbenItems = new LinkedHashMap<String, ActionListener>();
+        farbenItems.put("Weiss",    e->setColor(Color.WHITE));
+        farbenItems.put("Rot",      e->setColor(Color.RED));
+        farbenItems.put("Gruen",    e->setColor(Color.GREEN));
+        farbenItems.put("Blau",     e->setColor(Color.BLUE));
+        farbenItems.put("Gelb",     e->setColor(Color.YELLOW));
+        farbenItems.put("Schwarz",  e->setColor(Color.BLACK));
+        farbenItems.put("Pink",     e->setColor(Color.PINK));
+        return farbenItems;
+    }
+
+    private void setColor(final Color color) {
+        paintClone.setColor(color);
+    }
+    private void setForm( PaintClone.Form form) {
+        paintClone.setForm(form);
+    }
+
+
 }
--- Tag3_02PaintClone/src/de/gui/PaintClone.java
+++ Tag3_02PaintClone/src/de/gui/PaintClone.java
@@ -1,19 +1,171 @@
 package de.gui;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageFilter;
+import java.io.File;
+import java.text.Format;
 
 public class PaintClone extends Frame {
 
 
+    public enum Form {
+        LINE,
+        RECTANGLE,
+        OVAL,
+        CLEAR;
+    }
+
     private final static int SIZE = 1000;
+
+    private BufferedImage image = new BufferedImage(SIZE,SIZE,BufferedImage.TYPE_INT_RGB);
+    private Form form = Form.LINE;
+    private Color color = Color.WHITE;
+
 
     public PaintClone() {
         super("PaintClone");
         setSize(SIZE,SIZE);
-        setMenuBar(new MyMenuBar());
+        setResizable(false);
+        setBackground(Color.BLACK);
+        addWindowListener(new MyWindowListerener());
+        addMouseListener(new MyMouseListener());
+        setMenuBar(new MyMenuBar(this));
+    }
+
+    public Form getForm() {
+        return form;
+    }
+
+    public void setForm(final Form form) {
+        this.form = form;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(final Color color) {
+        this.color = color;
+    }
+
+    public void close() {
+        dispose();
+    }
+
+    public void save() {
+        var chooser = createFileChooser();
+        var selection = chooser.showSaveDialog(this);
+        if(selection == JFileChooser.APPROVE_OPTION) {
+            System.out.println("auswahl");
+        }
+
+    }
+
+    public void load() {
+        var chooser = createFileChooser();
+        var selection = chooser.showOpenDialog(this);
+    }
+
+
+
+    @Override
+    public void paint(final Graphics g) {
+        g.drawImage(image,0,0,this);
+    }
+
+    public void drawForm(final Point start, final Point end) {
+        Rectangle rectangle = createNormalizedRectangleFromCoordinates(start, end);
+        renderForm(rectangle);
+    }
+
+    private void renderForm(final Rectangle rectangle) {
+        Graphics graphics = image.getGraphics();
+        graphics.setColor(getColor());
+        switch (getForm()) {
+            case LINE-> graphics.drawLine(rectangle.x, rectangle.y, rectangle.x + rectangle.width, rectangle.y + rectangle.height);
+            case OVAL -> graphics.drawOval(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+            case RECTANGLE -> graphics.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+            case CLEAR -> graphics.clearRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        }
+        graphics.dispose();
+        repaint();
+    }
+
+    private JFileChooser createFileChooser() {
+
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Bitte Grafikdatei waehlen:");
+        chooser.addChoosableFileFilter(new FileTypeFilter("png", "Portable Network Graphic" ));
+        chooser.addChoosableFileFilter(new FileTypeFilter("gif", "Graphics Interchange Format" ));
+        chooser.addChoosableFileFilter(new FileTypeFilter("jpg", "JPEG" ));
+
+
+
+        return chooser;
+    }
+    private Rectangle createNormalizedRectangleFromCoordinates(final Point start, final Point end) {
+
+        var rectangle = new Rectangle();
+        rectangle.x = start.x<end.x ? start.x : end.x;
+        rectangle.y = start.y < end.y ?  start.y : end.y;
+        rectangle.width = Math.abs(end.x - start.x);
+        rectangle.height = Math.abs(end.y - start.y);
+        return rectangle;
+
     }
 
     public static void main(String[] args) {
         new PaintClone().setVisible(true);
+    }
+    private class MyMouseListener extends MouseAdapter {
+
+        private final Point startPoint = new Point();
+
+        @Override
+        public void mousePressed(final MouseEvent e) {
+            startPoint.move(e.getX(),e.getY());
+        }
+
+        @Override
+        public void mouseReleased(final MouseEvent e) {
+             drawForm(startPoint, new Point(e.getX(),e.getY()));
+        }
+
+
+    }
+
+    private class MyWindowListerener extends WindowAdapter {
+        @Override
+        public void windowClosing(final WindowEvent e) {
+            close();
+        }
+    }
+
+    private class FileTypeFilter extends FileFilter {
+        private String extension;
+        private String description;
+
+        public FileTypeFilter(String extension, String description) {
+            this.extension = extension;
+            this.description = description;
+        }
+
+        public boolean accept(File file) {
+            if (file.isDirectory()) {
+                return true;
+            }
+            return file.getName().endsWith(extension);
+        }
+
+        public String getDescription() {
+            return description + String.format(" (*%s)", extension);
+        }
     }
 }
